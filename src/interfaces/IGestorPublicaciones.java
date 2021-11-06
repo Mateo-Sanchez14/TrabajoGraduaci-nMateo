39623d126f194f4e44ba51a5915c9af70519e47d
Mateo <mateo-sanchez@hotmail.com>
2021-11-06 00:02:56 -0300

    Construcción: Agregar interface y gestor de publicaciones

---
+++ src/interfaces/IGestorPublicaciones.java
@@ -0,0 +1,43 @@
+package interfaces;
+
+import autores.modelos.Autor;
+import grupos.modelos.MiembroEnGrupo;
+import idiomas.modelos.Idioma;
+import lugares.modelos.Lugar;
+import palabrasclaves.modelos.PalabraClave;
+import publicaciones.modelos.Publicacion;
+import tipos.modelos.Tipo;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public interface IGestorPublicaciones {
+    //Constantes de mensajes
+    public final String EXITO = "Publicación creada/borrada con éxito";
+    public final String ERROR_TITULO = "El título de la publicación no puede ser nulo";
+    public final String ERROR_MIEMBRO = "El miembro no puede ser nulo";
+    public final String ERROR_FECHA = "La fecha no puede ser nula";
+    public final String ERROR_TIPO = "El tipo no puede ser nulo";
+    public final String ERROR_IDIOMA = "El idioma no puede ser nulo";
+    public final String ERROR_LUGAR = "El lugar no puede ser nulo";
+    public final String ERROR_PALABRAS = "Las palabras claves no pueden ser nulas";
+    public final String ERROR_ENLACE = "El enlace no puede ser nulo";
+    public final String ERROR_RESUMEN = "El resumen no puede ser nulo";
+    public final String PUBLICACION_DUPLICADA = "Ya existe una palabra clave con ese nombre ";
+    public final String PUBLICACION_INEXISTENTE = "No existe la publicación clave en específico";
+
+    public String nuevaPublicacion(String titulo, MiembroEnGrupo miembroEnGrupo,
+                                   LocalDate fechaPublicacion, Tipo tipo, Idioma idioma, Lugar lugar,
+                                   ArrayList<PalabraClave> palabrasClaves, String enlace, String resumen);
+    public String modificarPublicacion(Publicacion publicacion, MiembroEnGrupo miembroEnGrupo,
+                                       LocalDate fechaPublicacion, Tipo tipo, Idioma idioma, Lugar lugar,
+                                       ArrayList<PalabraClave> palabrasClaves, String enlace, String resumen);
+    public boolean hayPublicacionesConEstaPalabraClave(PalabraClave palabraClave);
+    public boolean hayPublicacionesConEsteLugar(Lugar lugar);
+    public boolean hayPublicacionesConEsteIdioma(Idioma idioma);
+    public boolean hayPublicacionesConEsteTipo(Tipo tipo);
+    public boolean hayPublicacionesConEsteAutor(Autor autor);
+    public boolean existeEstaPublicacion(Publicacion publicacion);
+    public ArrayList<Publicacion> verPublicaciones();
+    public Publicacion verPublicacion(String titulo);
+}
---
+++ src/publicaciones/modelos/GestorPublicaciones.java
@@ -0,0 +1,142 @@
+package publicaciones.modelos;
+
+import autores.modelos.Autor;
+import grupos.modelos.MiembroEnGrupo;
+import idiomas.modelos.Idioma;
+import interfaces.IGestorPublicaciones;
+import lugares.modelos.Lugar;
+import palabrasclaves.modelos.PalabraClave;
+import tipos.modelos.Tipo;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class GestorPublicaciones implements IGestorPublicaciones {
+    ArrayList<Publicacion> publicaciones = new ArrayList<>();
+
+    private static GestorPublicaciones gestor;
+
+    private GestorPublicaciones(){
+    }
+
+    public static GestorPublicaciones instanciar() {
+        if(gestor == null) {
+            gestor = new GestorPublicaciones();
+        }
+        return gestor;
+    }
+
+    public String nuevaPublicacion(String titulo, MiembroEnGrupo miembroEnGrupo,
+                                   LocalDate fechaPublicacion, Tipo tipo, Idioma idioma, Lugar lugar,
+                                   ArrayList<PalabraClave> palabrasClaves, String enlace, String resumen) {
+
+        if( (titulo == null) || titulo.trim().isEmpty())
+            return ERROR_TITULO;
+        if (miembroEnGrupo == null)
+            return ERROR_MIEMBRO;
+        if (fechaPublicacion == null)
+            return ERROR_FECHA;
+        if (tipo == null)
+            return ERROR_TIPO;
+        if (idioma == null)
+            return ERROR_IDIOMA;
+        if (lugar == null)
+            return ERROR_LUGAR;
+        if (palabrasClaves == null) //Consultar si puede ser una arraylist sin elementos
+            return ERROR_PALABRAS;
+        if ((enlace == null) || enlace.trim().isEmpty())
+            return ERROR_ENLACE;
+        if ((resumen == null) || resumen.trim().isEmpty())
+            return ERROR_RESUMEN;
+        Publicacion unaPublicacion = new Publicacion(titulo, miembroEnGrupo, fechaPublicacion, tipo,
+                                                     idioma, lugar, palabrasClaves, enlace, resumen);
+        if (publicaciones.contains(unaPublicacion))
+            return PUBLICACION_DUPLICADA;
+        publicaciones.add(unaPublicacion);
+        return EXITO;
+    }
+    public String modificarPublicacion(Publicacion publicacion, MiembroEnGrupo miembroEnGrupo,
+                                       LocalDate fechaPublicacion, Tipo tipo, Idioma idioma, Lugar lugar,
+                                       ArrayList<PalabraClave> palabrasClaves, String enlace, String resumen) {
+        if (miembroEnGrupo == null)
+            return ERROR_MIEMBRO;
+        if (fechaPublicacion == null)
+            return ERROR_FECHA;
+        if (tipo == null)
+            return ERROR_TIPO;
+        if (idioma == null)
+            return ERROR_IDIOMA;
+        if (lugar == null)
+            return ERROR_LUGAR;
+        if (palabrasClaves == null) //Consultar si puede ser una arraylist sin elementos
+            return ERROR_PALABRAS;
+        if ((enlace == null) || enlace.trim().isEmpty())
+            return ERROR_ENLACE;
+        if ((resumen == null) || resumen.trim().isEmpty())
+            return ERROR_RESUMEN;
+        if (publicacion == null)
+            return  PUBLICACION_INEXISTENTE;
+
+        publicacion.asignarMiembroEnGrupo(miembroEnGrupo);
+        publicacion.asignarFechaPublicacion(fechaPublicacion);
+        publicacion.asignarTipo(tipo);
+        publicacion.asignarIdioma(idioma);
+        publicacion.asignarLugar(lugar);
+        publicacion.asignarPalabrasClaves(palabrasClaves);
+        publicacion.asignarEnlace(enlace);
+        publicacion.asignarResumen(resumen);
+
+        return EXITO;
+    }
+    public boolean hayPublicacionesConEstaPalabraClave(PalabraClave palabraClave) {
+        for (Publicacion k : publicaciones) {
+            if (k.verPalabrasClaves().contains(palabraClave))
+                return true;
+        }
+        return false;
+    }
+    public boolean hayPublicacionesConEsteLugar(Lugar lugar) {
+        for (Publicacion k : publicaciones) {
+            if (k.verLugar().equals(lugar))
+                return true;
+        }
+        return false;
+    }
+    public boolean hayPublicacionesConEsteIdioma(Idioma idioma) {
+        for (Publicacion k : publicaciones) {
+            if (k.verIdioma().equals(idioma))
+                return true;
+        }
+        return false;
+    }
+    public boolean hayPublicacionesConEsteTipo(Tipo tipo) {
+        for (Publicacion k : publicaciones) {
+            if (k.verTipo().equals(tipo))
+                return true;
+        }
+        return false;
+    }
+    public boolean hayPublicacionesConEsteAutor(Autor autor) {
+        for (Publicacion k : publicaciones) {
+            if (k.verMiembroEnGrupo().verAutor().equals(autor))
+                return true;
+        }
+        return false;
+    }
+    public boolean existeEstaPublicacion(Publicacion publicacion) {
+        if (publicaciones.contains(publicacion))
+            return true;
+        return false;
+    }
+    public ArrayList<Publicacion> verPublicaciones() {
+        return publicaciones;
+    }
+    public Publicacion verPublicacion(String titulo) {
+        for (Publicacion k : publicaciones) {
+            if (k.verTitulo().equals(titulo)){
+                return k;
+            }
+        }
+        return null;
+    }
+}
