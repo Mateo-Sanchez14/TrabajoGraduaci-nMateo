150f86d27d771aa18d085bdf07011bc9b707288b
mateo <mateo-sanchez@hotmail.com>
2021-11-29 16:12:32 -0300

    Implementar persistencia

--- src/interfaces/IGestorPalabrasClaves.java
+++ src/interfaces/IGestorPalabrasClaves.java
@@ -12,6 +12,7 @@ public interface IGestorPalabrasClaves {
     public final String PALABRA_DUPLICADA = "Ya existe una palabra clave con ese nombre ";
     public final String PALABRA_CON_PUBLICACION = "No se puede borrar la palabra clave porque hay publicaciones con esa palabra clave";
     public final String PALABRA_INEXISTENTE = "No existe la palabra clave en específico";
+    public final String TEXTO = "./PalabrasClave.txt";
 
     //Métodos
     public String borrarPalabraClave(PalabraClave palabraClave);
--- src/palabrasclaves/modelos/GestorPalabrasClaves.java
+++ src/palabrasclaves/modelos/GestorPalabrasClaves.java
@@ -6,6 +6,7 @@ import publicaciones.modelos.GestorPublicaciones;
 import tipos.modelos.GestorTipos;
 import tipos.modelos.Tipo;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -15,7 +16,7 @@ public class GestorPalabrasClaves implements IGestorPalabrasClaves {
 
     private static GestorPalabrasClaves gestor;
 
-    private GestorPalabrasClaves () {
+    private GestorPalabrasClaves() {
     }
 
     public static GestorPalabrasClaves instanciar() {
@@ -31,17 +32,19 @@ public class GestorPalabrasClaves implements IGestorPalabrasClaves {
         if (this.palabrasClaves.contains(unaPalabra))
             return PALABRA_DUPLICADA;
         this.palabrasClaves.add(unaPalabra);
+        escribirArchivo();
         return EXITO;
     }
 
     public List<PalabraClave> verPalabrasClaves() {
+        this.leerArchivo();
         Collections.sort(this.palabrasClaves);
         return this.palabrasClaves;
     }
 
     public PalabraClave verPalabraClave(String nombre) {
         for (PalabraClave x : palabrasClaves) {
-            if (x.verNombre().equals(nombre)){
+            if (x.verNombre().equals(nombre)) {
                 return x;
             }
         }
@@ -56,13 +59,14 @@ public class GestorPalabrasClaves implements IGestorPalabrasClaves {
         if (gestorPublicaciones.hayPublicacionesConEstaPalabraClave(palabraClave))
             return PALABRA_CON_PUBLICACION;
         this.palabrasClaves.remove(palabraClave);
+        this.escribirArchivo();
         return EXITO;
     }
 
     @Override
     public List<PalabraClave> buscarPalabrasClaves(String nombre) {
         List<PalabraClave> palabrasEncontradas = new ArrayList<>();
-        for (PalabraClave x : this.palabrasClaves){
+        for (PalabraClave x : this.palabrasClaves) {
             if (x.verNombre().toLowerCase().contains(nombre.toLowerCase()))
                 palabrasEncontradas.add(x);
         }
@@ -78,5 +82,66 @@ public class GestorPalabrasClaves implements IGestorPalabrasClaves {
             }
         }
         return false;
+    }
+
+    private void leerArchivo() {
+        palabrasClaves.removeAll(palabrasClaves);
+        BufferedReader br = null;
+        File file = new File(TEXTO);
+
+        if (file.exists()) {
+            try {
+                FileReader fr = new FileReader(file);
+                br = new BufferedReader(fr);
+                String cadena;
+                while((cadena = br.readLine()) != null) {
+                    this.nuevaPalabraClave(cadena);
+                }
+            }
+            catch (IOException ioe) {
+                System.out.println("No se pudo leer el archivo de texto");
+            }
+            finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    }
+                    catch (IOException ioe) {
+                        ioe.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    private void escribirArchivo() {
+        BufferedWriter bw = null;
+        File file = new File(TEXTO);
+
+        try {
+            FileWriter fw = new FileWriter(file);
+            bw = new BufferedWriter(fw);
+            for(int i = 0; i < this.palabrasClaves.size(); i++) {
+                PalabraClave unaPalabra = this.palabrasClaves.get(i);
+                String cadena = unaPalabra.verNombre();
+
+                bw.write(cadena);
+                if (i < this.palabrasClaves.size() - 1)
+                    bw.newLine();
+            }
+        }
+        catch (IOException ioe) {
+            System.out.println("Error al escribir el archivo");
+        }
+        finally {
+            if (bw != null) {
+                try {
+                    bw.close();
+                }
+                catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+            }
+        }
     }
 }
